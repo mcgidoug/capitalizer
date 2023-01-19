0831bece793a9f092d5e853f878d8f706651209e
mcgidoug <mcgidoug@gmail.com>
2023-01-19 12:46:34 -0600

    added lowercase function

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 // query selectors
 let capButton = document.querySelector("#cap-button");
+let lowerButton = document.querySelector("#lower-button");
 
 // functions
 // ============================================================================
 function capitalizeText() {
   // Get the input field by its id
-  let inputField = document.getElementById("inputField");
+  let upperInputField = document.getElementById("upper-input-field");
   // Get the current text in the input field
-  let text = inputField.value;
+  let text = upperInputField.value;
   // Capitalize the text
   let capitalizedText = text.toUpperCase();
   // Update the input field with the capitalized text
-  inputField.value = capitalizedText;
+  upperInputField.value = capitalizedText;
+}
+
+function lowercaseText() {
+  // Get the input field by its id
+  let lowerInputField = document.getElementById("lower-input-field");
+  // Get the current text in the input field
+  let text = lowerInputField.value;
+  // Capitalize the text
+  let capitalizedText = text.toLowerCase();
+  // Update the input field with the capitalized text
+  lowerInputField.value = capitalizedText;
 }
 
 // event listeners
 // ============================================================================
 capButton.addEventListener("click", capitalizeText);
+lowerButton.addEventListener("click", lowercaseText);
